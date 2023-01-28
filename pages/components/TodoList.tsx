9a69740d8cbabd7fcdec8e65550b8572ec1aa5eb
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-01-28 14:30:29 +0900

    Change Todolist to .tsx

---
+++ pages/components/TodoList.tsx
@@ -0,0 +1,58 @@
+import {
+  Badge,
+  HStack,
+  IconButton,
+  Spacer,
+  StackDivider,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { FaTrash } from "react-icons/fa";
+
+type Todo = {
+  id: number;
+  body: string;
+};
+
+type Props = {
+  todos: Todo[];
+  deleteTodo: (id: number) => void;
+};
+
+const TodoList: React.FC<Props> = ({ todos, deleteTodo }) => {
+  if (!todos.length) {
+    return (
+      <Badge colorScheme="green" p="4" m="4" borderRadius="lg">
+        No Todos, yay!
+      </Badge>
+    );
+  }
+
+  return (
+    <VStack
+      divider={<StackDivider />}
+      borderColor="gray.100"
+      borderWidth="2px"
+      borderRadius="lg"
+      width="100%"
+      maxW={{ base: "90vw", sm: "80vw", lg: "50vw", xl: "40vw" }}
+      p="4"
+      alignItems="stretch"
+    >
+      {todos.map((todo) => (
+        <HStack key={todo.id}>
+          <Text>{todo.body}</Text>
+          <Spacer />
+          <IconButton
+            aria-label="Trash"
+            icon={<FaTrash />}
+            isRound={true}
+            onClick={() => deleteTodo(todo.id)}
+          />
+        </HStack>
+      ))}
+    </VStack>
+  );
+};
+
+export default TodoList;
